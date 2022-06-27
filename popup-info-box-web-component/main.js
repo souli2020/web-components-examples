5494beea1a091487a177228eed000c8c2879f9e4
M Souli <65006383+souli2020@users.noreply.github.com>
2022-06-27 13:56:59 +0100

    using getElementById instead of class constructor
    i found that its more easier to understand the shadow DOM topic in this example using the document.getElementById() instead of class Constructor

--- popup-info-box-web-component/main.js
+++ popup-info-box-web-component/main.js
@@ -1,81 +1,74 @@
-// Create a class for the element
-class PopUpInfo extends HTMLElement {
-  constructor() {
-    // Always call super first in constructor
-    super();
+ const popUpInfo = document.getElementById('popUpInfo')  
+         
+       //create a shadow root
+       const shadow = popUpInfo.attachShadow({mode:"open"})
+          //create spans
+          const wrapper = document.createElement('span')
+         wrapper.setAttribute('class', 'wrapper')
+         
+         const info = document.createElement('span')
+         info.setAttribute('class', 'info')
+         
+         const icon = document.createElement('span')
+         icon.setAttribute('class','icon')
+          icon.setAttribute('tabindex', 0);
+        
+         //get the attribute text and insert it to info
+         
+         const text = popUpInfo.getAttribute('data-text')
+         info.textContent = text
+          //insert icon*
+           let imgUrl 
+          if(popUpInfo.hasAttribute("img")){
+              imgUrl = popUpInfo.getAttribute('img')
+          }
+         else{imgUrl = "defaultpng.png"}
+     
+         //create the img 
+         const img = document.createElement('img')
+         img.src = imgUrl
+         
+         //appenf img to the icon span
+         icon.appendChild(img)
+         
+         
+        // create the css style
+        let style = document.createElement('style')
+         style.textContent = `
+                                .wrapper {
+                                position: relative;
+                            }
 
-    // Create a shadow root
-    const shadow = this.attachShadow({mode: 'open'});
+                            .info {
+                                font-size: 0.8rem;
+                                width: 200px;
+                                display: inline-block;
+                                border: 1px solid black;
+                                padding: 10px;
+                                background: grey;
+                                border-radius: 10px;
+                                opacity: 0;
+                                transition: 0.6s all;
+                                position: absolute;
+                                bottom: 20px;
+                                left: 10px;
+                                z-index: 3;
+                                color: white;
+                            }
 
-    // Create spans
-    const wrapper = document.createElement('span');
-    wrapper.setAttribute('class', 'wrapper');
+                            img {
+                                width: 1.2rem;
+                            }
 
-    const icon = document.createElement('span');
-    icon.setAttribute('class', 'icon');
-    icon.setAttribute('tabindex', 0);
-
-    const info = document.createElement('span');
-    info.setAttribute('class', 'info');
-
-    // Take attribute content and put it inside the info span
-    const text = this.getAttribute('data-text');
-    info.textContent = text;
-
-    // Insert icon
-    let imgUrl;
-    if(this.hasAttribute('img')) {
-      imgUrl = this.getAttribute('img');
-    } else {
-      imgUrl = 'img/default.png';
-    }
-
-    const img = document.createElement('img');
-    img.src = imgUrl;
-    icon.appendChild(img);
-
-    // Create some CSS to apply to the shadow dom
-    const style = document.createElement('style');
-    console.log(style.isConnected);
-
-    style.textContent = `
-      .wrapper {
-        position: relative;
-      }
-
-      .info {
-        font-size: 0.8rem;
-        width: 200px;
-        display: inline-block;
-        border: 1px solid black;
-        padding: 10px;
-        background: white;
-        border-radius: 10px;
-        opacity: 0;
-        transition: 0.6s all;
-        position: absolute;
-        bottom: 20px;
-        left: 10px;
-        z-index: 3;
-      }
-
-      img {
-        width: 1.2rem;
-      }
-
-      .icon:hover + .info, .icon:focus + .info {
-        opacity: 1;
-      }
-    `;
-
-    // Attach the created elements to the shadow dom
-    shadow.appendChild(style);
-    console.log(style.isConnected);
-    shadow.appendChild(wrapper);
-    wrapper.appendChild(icon);
-    wrapper.appendChild(info);
-  }
-}
-
-// Define the new element
-customElements.define('popup-info', PopUpInfo);
+                            .icon:hover + .info, .icon:focus + .info {
+                                opacity: 1;
+                            }
+        
+        
+        `
+         
+         
+         shadow.appendChild(style)
+         shadow.appendChild(wrapper)
+         wrapper.appendChild(icon)
+         wrapper.appendChild(info)
