e40eda56f9935d7e582527ef82a1427fb20566b3
M Souli <65006383+souli2020@users.noreply.github.com>
2022-06-27 15:51:49 +0100

    removing class Constructor + external Style

--- word-count-web-component/main.js
+++ word-count-web-component/main.js
@@ -1,36 +1,35 @@
-// Create a class for the element
-class WordCount extends HTMLParagraphElement {
-  constructor() {
-    // Always call super first in constructor
-    super();
+const wcParent = document.getElementById('parent')
+    const word = document.getElementById("word-count")
+    
+       //count words in element's parent element
+        function countWords(node){
+        const text = node.innerText || node.textContent;
+        return text.trim().split(/\s+/g).filter(a => a.trim().length > 0).length;
+        }
+       
+        const count = `Words:${countWords(wcParent)}`;
 
-    // count words in element's parent element
-    const wcParent = this.parentNode;
+        // Create a shadow root
+        const shadow = word.attachShadow({mode: 'open'});
 
-    function countWords(node){
-      const text = node.innerText || node.textContent;
-      return text.trim().split(/\s+/g).filter(a => a.trim().length > 0).length;
-    }
+        // Create text node and add word count to it
+        const text = document.createElement('span');
+        text.setAttribute('class', "text")
+        text.innerHTML = count;
+    
+        //create external css for the shadow
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'style.css');
 
-    const count = `Words: ${countWords(wcParent)}`;
+        // Append the external css style to the shadow 
+        shadow.appendChild(linkElem);
+        // Append the text element to the shadow root
+        shadow.appendChild(text);
+    
 
-    // Create a shadow root
-    const shadow = this.attachShadow({mode: 'open'});
-
-    // Create text node and add word count to it
-    const text = document.createElement('span');
-    text.textContent = count;
-
-    // Append it to the shadow root
-    shadow.appendChild(text);
-
-    // Update count when element content changes
-    setInterval(function() {
-      const count = `Words: ${countWords(wcParent)}`;
-      text.textContent = count;
-    }, 200);
-  }
-}
-
-// Define the new element
-customElements.define('word-count', WordCount, { extends: 'p' });
+        // Update count when element content changes
+        setInterval(function() {
+        const count = `Words: ${countWords(wcParent)}`;
+        text.textContent = count;
+        }, 200);
